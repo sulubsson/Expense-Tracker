feefa50535e7d56d7a62d66b1c5f782b9668793b
ahmed ali <sulubsson@gmail.com>
2021-12-13 22:34:16 +0100

    first commit of the project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+
+import Header from'./Components/Header'
+import Balance from './Components/Balance'
+import { IncomeExpenses } from './Components/IncomeExpenses'
+import {TransactionList} from './Components/TransactionList'
+import AddTransaction from './Components/AddTransaction'
+
+function App(props) {
+    return (
+        <div>
+            <Header/>
+            <div className="container">
+                <Balance/>
+                <IncomeExpenses/>
+                <TransactionList/>
+                <AddTransaction/>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default App
+
